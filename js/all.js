074869aff54ee8b62ee4711d92fcffa73f789f1a
Reiracode <reira.igg@gmail.com>
2021-07-09 18:31:03 +0800

    update login/member

--- js/all.js
+++ js/all.js
@@ -1,18 +1,12 @@
-// {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>   */}
-$(document).ready(function() {
-  AOS.init();
+$(document).ready(function () {
+
+
 // navbar
   const navBar = document.querySelector(".menu");
   const dropNav = document.querySelector("ul.navlink");
-  navBar.addEventListener("click", function() {
+  navBar.addEventListener("click", function () {
+    navBar.classList.toggle("active");
     dropNav.classList.toggle("active");
   });
-
-
-
-
-  // $(window).load(function() {
-  //   $("#product_content").addClass("loading");
-  // });
 
 });
